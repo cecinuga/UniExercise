dfbbb56d35237cf91ee8f7616254db3dd9eb741d
Marchetti Matteo <masterammo01@gmail.com>
2022-06-14 14:34:04 +0200

    14/06/2022

--- ts/ts1.js
+++ ts/ts1.js
@@ -1,4 +1,79 @@
 function sommaCifre(v) {
-    if (Math.floor(v / 10) == 0) return v;
+    if (Math.floor(v / 10) == 0)
+        return v;
     return v % 10 + sommaCifre(Math.floor(v / 10));
-};console.log(sommaCifre(4679132544));
+}
+;
+console.log(sommaCifre(5));
+var CodaL = /** @class */ (function () {
+    function CodaL() {
+        this.dimensione = 0;
+    }
+    CodaL.prototype.costructor = function () {
+        this.head = null;
+        this.tail = null;
+    };
+    CodaL.prototype.enqueue = function (nuovoNodo) {
+        if (this.head != null) {
+            this.head.prev = nuovoNodo;
+        }
+        else {
+            this.tail = nuovoNodo;
+        }
+        nuovoNodo.next = this.head;
+        nuovoNodo.prev = null;
+        this.head = nuovoNodo;
+        this.dimensione++;
+        return this.dimensione;
+    };
+    CodaL.prototype.dequeue = function () {
+        if (this.tail == null) {
+            return -1;
+        }
+        else {
+            this.tail = this.tail.prev;
+            if (this.tail != null)
+                this.tail.next = null;
+            this.dimensione--;
+            return this.dimensione;
+        }
+    };
+    CodaL.prototype.enqueueT = function (nuovoNodo) {
+        if (this.tail == null) {
+            this.head = nuovoNodo;
+            this.tail = nuovoNodo;
+        }
+        else {
+            this.tail.next = nuovoNodo;
+            nuovoNodo.prev = this.tail;
+            nuovoNodo.next = null;
+            this.tail = nuovoNodo;
+        }
+        this.dimensione++;
+        return this.dimensione;
+    };
+    CodaL.prototype.size = function () { return this.dimensione; };
+    CodaL.prototype.print = function () {
+        var temp = this.tail;
+        var s = "";
+        while (temp != null) {
+            s = s + " " + temp.value;
+            temp = temp.prev;
+        }
+        return s;
+    };
+    return CodaL;
+}());
+var q = new CodaL();
+var n1 = { value: 5, next: null, prev: null };
+var n2 = { value: 2, next: null, prev: null };
+var n3 = { value: 4, next: null, prev: null };
+var n4 = { value: 9, next: null, prev: null };
+var n5 = { value: 8, next: null, prev: null };
+q.enqueue(n1);
+q.enqueue(n2);
+q.enqueue(n3);
+q.enqueue(n4);
+q.enqueue(n5);
+q.size();
+console.log(q.print());
--- ts/ts1.ts
+++ ts/ts1.ts
@@ -1,6 +1,79 @@
 function sommaCifre(v:number):number{
     if(Math.floor(v/10)==0) return v;
     return v%10+sommaCifre(Math.floor(v/10));
+};console.log(sommaCifre(5))
+
+interface Nodo {
+    value: number;
+    next: Nodo | null;
+    prev: Nodo | null;
 }
 
-console.log(sommaCifre(5))
+class CodaL {
+    head: Nodo | null;
+    tail: Nodo | null;
+    dimensione: number = 0;
+    
+    costructor(){
+        this.head = null;
+        this.tail = null;
+    }
+    enqueue(nuovoNodo: Nodo): number {
+        if(this.head != null){
+            this.head.prev = nuovoNodo;
+         } else { this.tail = nuovoNodo; }
+        nuovoNodo.next = this.head;
+        nuovoNodo.prev = null;
+        this.head = nuovoNodo;
+        this.dimensione++;
+        return this.dimensione;
+    }
+    dequeue(): number {
+        if( this.tail == null ){ return -1; }
+        else { 
+            this.tail = this.tail.prev;
+            if(this.tail !=null) this.tail.next = null;
+            this.dimensione--;
+            return this.dimensione;
+        }
+    }
+    enqueueT(nuovoNodo: Nodo): number {
+        if(this.tail == null){
+            this.head = nuovoNodo;
+            this.tail = nuovoNodo;
+        } else { 
+            this.tail.next = nuovoNodo;
+            nuovoNodo.prev = this.tail;
+            nuovoNodo.next = null;
+            this.tail = nuovoNodo;
+        }
+        this.dimensione++;
+        return this.dimensione;
+    }
+    size(): number { return this.dimensione; }
+    print(): string { 
+        let temp = this.tail;
+        let s: string = "";
+        while(temp!=null){
+            s = s+" "+temp.value;
+            temp = temp.prev;
+        }
+        return s;
+    }
+}
+
+var q = new CodaL();
+let n1: Nodo = {value:5, next:null, prev:null}
+let n2: Nodo = {value:2, next:null, prev:null}
+let n3: Nodo = {value:4, next:null, prev:null}
+let n4: Nodo = {value:9, next:null, prev:null}
+let n5: Nodo = {value:8, next:null, prev:null}
+
+q.enqueue(n1);
+q.enqueue(n2);
+q.enqueue(n3);
+q.enqueue(n4);
+q.enqueue(n5);
+
+q.size();
+console.log(q.print());
