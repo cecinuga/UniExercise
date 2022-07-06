85b5cc32faa0610442df1beaf8743be86fdaa7f1
Marchetti Matteo <=>
2022-07-06 15:27:07 +0200

    ...

---
+++ ts/l1.js
@@ -0,0 +1,26 @@
+function somma(a, b) {
+    return a + b;
+}
+var Greeter = /** @class */ (function () {
+    function Greeter(message) {
+        this.greeting = message;
+    }
+    Greeter.prototype.greet = function () {
+        return 'Hello, ' + this.greeting;
+    };
+    return Greeter;
+}());
+var greeter = new Greeter("world");
+console.log(greeter.greet());
+var tupla = ['ciao', 5];
+console.log(tupla);
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 5] = "Green";
+    Color[Color["Blue"] = 6] = "Blue";
+})(Color || (Color = {}));
+var g = Color.Green;
+var b = Color.Blue;
+console.log(g);
+console.log(Color[6]);
---
+++ ts/l1.ts
@@ -0,0 +1,30 @@
+function somma(a:number, b:number):number {
+    return a+b; 
+}
+
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet(): string { 
+        return 'Hello, '+this.greeting;
+    }
+
+}
+
+let greeter: Greeter = new Greeter("world");
+console.log(greeter.greet())
+
+let tupla: [string, number] = ['ciao', 5];
+console.log(tupla)
+
+enum Color{
+    Red, 
+    Green=5, 
+    Blue,
+}
+let g: Color = Color.Green
+let b: Color = Color.Blue
+console.log(g)
+console.log(Color[6])
